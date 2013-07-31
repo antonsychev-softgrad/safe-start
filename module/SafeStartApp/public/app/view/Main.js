136ade4c455db34049432a6300c0f09a363e8ed5
artpo <artem.ponomarenko@ocsico.com>
2013-07-31 16:49:44 +0300

    sencha touch get start app

---
+++ module/SafeStartApp/public/app/view/Main.js
@@ -0,0 +1,43 @@
+Ext.define('SafeStartApp.view.Main', {
+    extend: 'Ext.tab.Panel',
+    xtype: 'main',
+    requires: [
+        'Ext.TitleBar',
+        'Ext.Video'
+    ],
+    config: {
+        tabBarPosition: 'bottom',
+
+        items: [
+            {
+                title: 'Welcome',
+                iconCls: 'home',
+
+                styleHtmlContent: true,
+                scrollable: true,
+
+                items: {
+                    docked: 'top',
+                    xtype: 'titlebar',
+                    title: 'Welcome to Safe Start App'
+                },
+
+                html: [
+                    "content here"
+                ].join("")
+            },
+            {
+                title: 'Get Started',
+                iconCls: 'action',
+
+                items: [
+                    {
+                        docked: 'top',
+                        xtype: 'titlebar',
+                        title: 'Getting Started'
+                    }
+                ]
+            }
+        ]
+    }
+});
