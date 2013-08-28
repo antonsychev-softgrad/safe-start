fa67167899d3f36a4a7383d6f86663ae174af1db
d.sidorov <sdsdevil@ya.ru>
2013-08-28 14:08:34 +0300

    test

--- module/SafeStartApi/src/SafeStartApi/Controller/ProcessDataController.php
+++ module/SafeStartApi/src/SafeStartApi/Controller/ProcessDataController.php
@@ -7,11 +7,8 @@ use SafeStartApi\Base\RestrictedAccessRestController;
 class ProcessDataController extends RestrictedAccessRestController
 {
     public function uploadImagesAction() {
-        $return = null;
-        if ($this->getRequest()->isPost()) {
-            $return = $this->UploadPlugin(array('param_name'=>'image'))->post();
-        }
 
+        $return = $this->UploadPlugin(array('param_name'=>'image'))->post();
         $this->answer = array(
             'uploadInfo' => $return,
         );
