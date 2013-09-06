c44171e60b7d172cbc4b2e29b42b7bf516eba8f3
artpo <artem.ponomarenko@ocsico.com>
2013-09-06 15:28:39 +0300

    get default checklist

--- module/SafeStartApi/src/SafeStartApi/Controller/InfoController.php
+++ module/SafeStartApi/src/SafeStartApi/Controller/InfoController.php
@@ -9,40 +9,35 @@ use SafeStartApi\Base\RestController;
 
 class InfoController extends RestController
 {
-    protected static function getFileByDirAndName($dir, $tosearch)
+    public function getImageAction()
     {
-        if (file_exists($dir) && is_dir($dir)) {
-            $flags = \FilesystemIterator::SKIP_DOTS | \FilesystemIterator::FOLLOW_SYMLINKS;
-            $iterator = new \RecursiveDirectoryIterator($dir, $flags);
-            $iterator = new \RecursiveIteratorIterator($iterator, \RecursiveIteratorIterator::SELF_FIRST, \RecursiveIteratorIterator::CATCH_GET_CHILD);
-            foreach ($iterator as $file) {
-                $tosearch = urldecode($tosearch);
-                $fileInfo = pathinfo($file);
-                if (($fileInfo['filename'] == $tosearch) || ($fileInfo['basename'] == $tosearch)) {
-                    return $file;
-                }
+        $hash = $this->params('hash');
+        $size = $this->params('size');
+        if (empty($hash)) return false;
+        //todo: почекать все расширения изображений (jpg, png); есои полный размер есть а $size нету нужно его создать
+        if (file_exists($this->getFileSystemPath('data/users/') . $hash . $size . '.jpg')) {
+            $fileSizeInfo = @getimagesize($this->getFileSystemPath('data/users/') . $hash . $size . '.jpg');
+            if ($fileSizeInfo) { // it`s image
+                header('Content-Type: ' . $fileSizeInfo['mime']);
+                header('Content-Length: ' . filesize($this->getFileSystemPath('data/users/') . $hash . $size . '.jpg'));
+                echo file_get_contents($this->getFileSystemPath('data/users/') . $hash . $size . '.jpg');
             }
         }
-        return null;
+        return false;
     }
 
-    protected function get_user_id()
+    public function getDefaultChecklistAction()
     {
-        $user_folder = '';
-        try {
-            if (isset($this->authService)) {
-                if ($this->authService->hasIdentity()) {
-                    $user = $this->authService->getStorage()->read();
-                    $user_folder = "" . $user->getId() . "/";
-                }
-            }
-        } catch (\Exception $e) {
-
-        }
-        return $user_folder;
+        $query = $this->em->createQuery('SELECT f FROM SafeStartApi\Entity\DefaultField f WHERE f.deleted = 0 AND f.enabled = 1');
+        $items = $query->getResult();
+        $checklist = $this->GetDataPlugin()->buildChecklist($items);
+        $this->answer = array(
+            'checklist' => $checklist,
+        );
+        return $this->AnswerPlugin()->format($this->answer);
     }
 
-    protected function get_full_path($fEndPath = null)
+    protected function getFileSystemPath($fEndPath = null)
     {
         $root = $_SERVER['DOCUMENT_ROOT'];
         if (!file_exists($root . "/init_autoloader.php")) {
@@ -69,22 +64,5 @@ class InfoController extends RestController
         }
 
         return $returnFolder;
-    }
-
-    public function getImageAction()
-    {
-        $hash = $this->params('hash');
-        $size = $this->params('size');
-        if (empty($hash)) return false;
-        //todo: почекать все расширения изображений (jpg, png); есои полный размер есть а $size нету нужно его создать
-        if (file_exists($this->get_full_path('data/users/') . $hash . $size . '.jpg')) {
-            $fileSizeInfo = @getimagesize($this->get_full_path('data/users/') . $hash . $size . '.jpg');
-            if ($fileSizeInfo) { // it`s image
-                header('Content-Type: ' . $fileSizeInfo['mime']);
-                header('Content-Length: ' . filesize($this->get_full_path('data/users/') . $hash . $size . '.jpg'));
-                echo file_get_contents($this->get_full_path('data/users/') . $hash . $size . '.jpg');
-            }
-        }
-        return false;
     }
 }
