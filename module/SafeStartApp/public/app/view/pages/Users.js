63d4d75df7e00264d248135b45994fb438d7084f
Sergey.Chizh <Sergey.Chizh@ocsico.com>
2013-10-31 18:03:49 +0300

    sencha new design changes

--- module/SafeStartApp/public/app/view/pages/Users.js
+++ module/SafeStartApp/public/app/view/pages/Users.js
@@ -87,7 +87,7 @@ Ext.define('SafeStartApp.view.pages.Users', {
                             flex: 1
                         }, {
                             iconCls: 'arrow_left',
-                            height: 20,
+                            //height: 22,
                             cls: 'sfa-collapse',
                             iconMask: true,
                             handler: function (btn) {
@@ -132,7 +132,7 @@ Ext.define('SafeStartApp.view.pages.Users', {
                     xtype: 'toolbar',
                     items: [{
                         iconCls: 'arrow_right',
-                        height: 20,
+                        //height: 22,
                         iconMask: true,
                         handler: function() {
                             var panel = this.up('SafeStartLeftContainer');
