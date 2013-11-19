435effdeae96f1543521acc8c0a1b7766a23cfe7
artpo <artem.ponomarenko@ocsico.com>
2013-11-19 14:50:45 +0300

    test

--- module/SafeStartApi/src/SafeStartApi/Controller/Plugin/InspectionPdfPlugin.php
+++ module/SafeStartApi/src/SafeStartApi/Controller/Plugin/InspectionPdfPlugin.php
@@ -368,7 +368,7 @@ class InspectionPdfPlugin extends \SafeStartApi\Controller\Plugin\AbstractPdfPlu
             }
             if ($field->type != 'group') {
                 if ($field->type == 'datePicker' && isset($fieldsDataValues[$field->id]) && !empty($fieldsDataValues[$field->id]) && $fieldsDataValues[$field->id] != "null") {
-                    $value = !empty($fieldsDataValues[$field->id]) ? date($this->getController()->moduleConfig['params']['date_format'], (int)$fieldsDataValues[$field->id]) : '-';
+                    $value = $fieldsDataValues[$field->id]; //!empty($fieldsDataValues[$field->id]) ? date($this->getController()->moduleConfig['params']['date_format'], (int)$fieldsDataValues[$field->id]) : '-';
                 } else {
                     $value = (isset($fieldsDataValues[$field->id]) && !empty($fieldsDataValues[$field->id])) ? $fieldsDataValues[$field->id] : '-';
                 }
