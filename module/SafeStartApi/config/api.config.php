8242f30155e3264b6782014262279cb8c6e74a2d
d.sidorov <sdsdevil@ya.ru>
2015-04-07 11:17:12 +0300

    update composer require

--- module/SafeStartApi/config/api.config.php
+++ module/SafeStartApi/config/api.config.php
@@ -2,8 +2,8 @@
 
 $api = array(
     'params' => array(
-        'version' => '1.0-beta',
-        'site_url' => 'http://database.safestartinspections.com',
+        'version' => '2.0-beta',
+        'site_url' => 'http://database.safestartinspections.com:81',
         'email_static_content_url' => 'https://s3-us-west-2.amazonaws.com/safe-start/emails/',
         'date_format' => 'd/m/Y',
         'time_format' => 'H:i',
