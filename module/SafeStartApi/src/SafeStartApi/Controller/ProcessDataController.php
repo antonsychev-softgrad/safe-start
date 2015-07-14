58db586540dcb567456a4ec81f5659b3a56c4ab8
d.sidorov <sdsdevil@ya.ru>
2015-07-14 13:39:36 +0300

    fix get checklist pdf

--- module/SafeStartApi/src/SafeStartApi/Controller/ProcessDataController.php
+++ module/SafeStartApi/src/SafeStartApi/Controller/ProcessDataController.php
@@ -64,19 +64,28 @@ class ProcessDataController extends PublicAccessRestController
 
     public function generatePdfAction()
     {
-        $checkListId = (int)$this->params('id');
+        $checkListId = (string)$this->params('id');
 
-        $checkList = null;
-        $query = $this->em->createQuery("SELECT cl FROM SafeStartApi\Entity\CheckList cl WHERE cl.id = :id OR cl.hash = :hash");
-        $query->setParameters(array('id' => $checkListId, 'hash' => $checkListId));
-        $queryResult = $query->getResult();
-        if (is_array($queryResult) && !empty($queryResult) && isset($queryResult[0]))  $checkList = $queryResult[0];
-
-        if (!$checkList) return $this->getController()->_showNotFound('Requested inspection not found.');
+        $qb = $this->em->createQueryBuilder();
+        $qb->select('e')
+            ->from('SafeStartApi\Entity\CheckList', 'e')
+            ->where(
+                $qb->expr()->eq('e.id', ":id")
+            )
+            ->orWhere(
+                $qb->expr()->eq('e.hash', ":hash")
+            )
+            ->setParameters(array(
+                'id' => $checkListId,
+                'hash' => $checkListId));
+        $checkList = $qb->getQuery()->getOneOrNullResult();
+        if (!$checkList)
+            return $this->getController()->_showNotFound('Requested inspection not found.');
 
         $link = $checkList->getPdfLink();
         $path = '';//$this->inspectionPdf()->getFilePathByName($link);
-        if (!$link || !file_exists($path)) $path = $this->inspectionPdf()->create($checkList);
+        if (!$link || !file_exists($path))
+            $path = $this->inspectionPdf()->create($checkList);
 
         header("Content-Disposition: inline; filename={$checkList->getPdfLink()}");
         header("Content-type: application/x-pdf");
